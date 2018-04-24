2e3d657e795cdbec644565c53c5021a7774c933e
MartkCz <antik494@gmail.com>
2018-04-24 13:22:10 +0200

    removed type hint

--- src/Helpers.php
+++ src/Helpers.php
@@ -4,7 +4,7 @@ namespace WebChemistry\Forms\Controls;
 
 class Helpers {
 
-	public static function createButtonName(int $copyCount): string {
+	public static function createButtonName($copyCount) {
 		return Multiplier::SUBMIT_CREATE_NAME . ($copyCount === 1 ? '' : $copyCount);
 	}
 
