182ffadbfff5144f10ab65a649c65207e2133d13
Milan Kocourek <milan.kocourek@movisio.com>
2020-12-02 09:52:21 +0100

    Set default values fix for nested multipliers.

--- src/Multiplier.php
+++ src/Multiplier.php
@@ -476,6 +476,7 @@ class Multiplier extends Container
     private function createComponents(ComponentResolver $resolver): void
     {
         $containers = [];
+        $containerDefaults = $this->createContainer()->getValues('array');
 
         // Components from httpData
         if ($this->isFormSubmitted()) {
@@ -483,7 +484,7 @@ class Multiplier extends Container
                 $containers[] = $container = $this->addCopy($number);
 
                 /** @var BaseControl $control */
-                foreach ($container->getControls() as $control) {
+                foreach ($container->getComponents(false, IControl::class) as $control) {
                     $control->loadHttpData();
                 }
             }
@@ -500,6 +501,7 @@ class Multiplier extends Container
             $copyNumber = $this->copyNumber;
             while ($copyNumber > 0 && $this->isValidMaxCopies() && $this->totalCopies < $this->minCopies) {
                 $containers[] = $container = $this->addCopy();
+                $container->setValues($containerDefaults);
                 $copyNumber--;
             }
         }
@@ -514,7 +516,7 @@ class Multiplier extends Container
             $count = $resolver->getCreateNum();
             while ($count > 0 && $this->isValidMaxCopies()) {
                 $this->noValidate[] = $containers[] = $container = $this->addCopy();
-                $container->setValues($this->createContainer()->getValues('array'));
+                $container->setValues($containerDefaults);
                 $count--;
             }
         }
