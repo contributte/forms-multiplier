contributte/forms-multiplier

commit 45cf2a8ed2ea230686db813ba93fcd459c2f35b7

Fix PHPStan issue after nette/forms added more types
https://github.com/nette/forms/commit/0cd5069a1737f2438ce23a097f7b6e8f6441e9c1 added more types but that is overly broad for our usage pattern. We need to cast it to proper value until the return type is clarified upstream.

Also use `$this->getForm()` instead of `$this->form` since the former is already checked not to be `null` by `$this->isFormSubmitted()`.

src/Multiplier.php
@@ -390,7 +390,9 @@ class Multiplier extends Container
 	protected function loadHttpData(): void
 	{
 		if ($this->isFormSubmitted()) {
-			$httpData = Arrays::get($this->form->getHttpData(), $this->getHtmlName(), []);
+			/** @var array<mixed> The other types from the union can only be returned when the htmlName argument is passed. https://github.com/nette/forms/pull/333 */
+			$httpData = $this->getForm()->getHttpData();
+			$httpData = Arrays::get($httpData, $this->getHtmlName(), []);
 			$this->resolver = new ComponentResolver($httpData ?? [], $this->maxCopies, $this->minCopies);
 		}
 	}

commit bfe52ebf24ea9df3a1eed55eabc84fdfe1f35043

Fix coding style
This was accidentally broken in 45cf2a8ed2ea230686db813ba93fcd459c2f35b7

src/Multiplier.php
@@ -390,7 +390,7 @@ class Multiplier extends Container
 	protected function loadHttpData(): void
 	{
 		if ($this->isFormSubmitted()) {
-			/** @var array<mixed> The other types from the union can only be returned when the htmlName argument is passed. https://github.com/nette/forms/pull/333 */
+			/** @var array<mixed> $httpData The other types from the union can only be returned when the htmlName argument is passed. https://github.com/nette/forms/pull/333 */
 			$httpData = $this->getForm()->getHttpData();
 			$httpData = Arrays::get($httpData, $this->getHtmlName(), []);
 			$this->resolver = new ComponentResolver($httpData ?? [], $this->maxCopies, $this->minCopies);